78ec2b11d2dc5e48e71aeb4e4987789573d68108
Josh <88961456+JoshRiang@users.noreply.github.com>
2022-04-12 21:15:17 +0800

    Add files via upload

--- js/siren.js
+++ js/siren.js
@@ -3,33 +3,94 @@ var Yelp = new Audio('https://github.com/JoshRiang/siren.github.io/raw/main/audi
 var Phaser = new Audio('https://github.com/JoshRiang/siren.github.io/raw/main/audio/POLICE_WARNING.mp3');
 var Horn = new Audio('https://github.com/JoshRiang/siren.github.io/raw/main/audio/AIRHORN_EQD.mp3');
 var HighLow = new Audio('https://github.com/JoshRiang/siren.github.io/raw/main/audio/AMBULANCE_WARNING.mp3');
+var toggle = false;
 
+// General Function
 function stopAudio(audio) {
     audio.pause();
     audio.currentTime = 0;
 };
 
+function toggleButton() {
+    var button = document.getElementById("toggle-btn")
+    if (toggle == false) {
+        toggle = true;  
+        button.style.backgroundColor = "green";
+    } else {
+        toggle = false;  
+        button.style.backgroundColor = "red";
+    }
+};
+
+// WAIL
+var WailTekan = false
+
 function HoldWail() {
-    Wail.play();
-    Wail.addEventListener('ended', function() {
+    if (toggle == false) {
         Wail.play();
-    }, false);    
+        Wail.addEventListener('ended', function() {
+            Wail.play();
+        }, false);  
+    }
+}
+
+function ToggleWail() {
+    if (toggle == true && WailTekan == false) {
+        stopAudio(Yelp)
+        YelpTekan = false;
+        stopAudio(Phaser)
+        PhaserTekan = false;        
+        WailTekan = true;
+        Wail.currentTime = 0;
+        Wail.play();
+        Wail.addEventListener('ended', function() {
+            Wail.play
+        }, false);  
+    } else if (WailTekan == true) {
+        WailTekan = false;
+        stopAudio(Wail);
+    }
 };
 
 function StopWail() {
     stopAudio(Wail);
 }
 
+// YELP
+var YelpTekan = false
+
 function HoldYelp() {
-    Yelp.play();
-    Yelp.addEventListener('ended', function() {
+    if (toggle == false) {
         Yelp.play();
-    }, false);    
+        Yelp.addEventListener('ended', function() {
+            Yelp.play();
+        }, false);  
+    }
 }
+
+function ToggleYelp() {
+    if (toggle == true && YelpTekan == false) {
+        stopAudio(Wail)
+        WailTekan = false;
+        stopAudio(Phaser)
+        PhaserTekan = false;
+        YelpTekan = true
+        Yelp.play();
+        Yelp.addEventListener('ended', function() {
+            Yelp.play();
+        }, false);  
+    } else if (YelpTekan == true){
+        YelpTekan = false;
+        stopAudio(Yelp);
+    }
+} 
 
 function StopYelp() {
     stopAudio(Yelp);
 }
+
+// PHASER
+var PhaserTekan = false
 
 function HoldPhaser() {
     Phaser.play();
@@ -38,20 +99,28 @@ function HoldPhaser() {
     }, false);    
 }
 
+function TogglePhaser() {
+    if (toggle == true && PhaserTekan == false) {
+        stopAudio(Wail)
+        WailTekan = false
+        stopAudio(Yelp)
+        YelpTekan = false
+        PhaserTekan = true
+        Phaser.play();
+        Phaser.addEventListener('ended', function() {
+            Phaser.play();
+        }, false);  
+    } else if (PhaserTekan == true){
+        PhaserTekan = false;
+        stopAudio(Phaser);
+    }
+}
+
 function StopPhaser() {
     stopAudio(Phaser);
 }
 
-function HoldHorn() {
-    Horn.play();
-    Horn.addEventListener('ended', function() {
-        Horn.play();
-    }, false);    
-}
-
-function StopHorn() {
-    stopAudio(Horn);
-}
+// HIGH LOW
 
 function HoldHighLow() {
     HighLow.play();
@@ -60,6 +129,52 @@ function HoldHighLow() {
     }, false);    
 }
 
+var HighLowTekan = false
+
+function ToggleHighLow() {
+    if (toggle == true && HighLowTekan == false) {
+        HighLowTekan = true
+        stopAudio(Wail)
+        WailTekan = false;
+        stopAudio(Yelp)
+        YelpTekan = false;
+        stopAudio(Phaser)
+        PhaserTekan = 0;
+        HighLow.play();
+        HighLow.addEventListener('timeupdate', function(){
+            var bufferHighLow = .24
+            if(this.currentTime > this.duration - bufferHighLow){
+                this.currentTime = 0
+                this.play()
+            }
+        });
+    } else if (HighLowTekan == true){
+        HighLowTekan = false;
+        stopAudio(HighLow);
+    }
+}
+
 function StopHighLow() {
     stopAudio(HighLow);
+}
+
+// HORN
+Horn.loop = true
+
+function HoldHorn() {
+    Horn.play();
+    Wail.pause();
+    Yelp.pause();
+    Phaser.pause();
+}
+
+function StopHorn() {
+    stopAudio(Horn);
+    if (WailTekan == true) {
+        Wail.play()
+    } else if (YelpTekan == true) {
+        Yelp.play()
+    } else if (PhaserTekan == true) {
+        Phaser.play()
+    }
 }
