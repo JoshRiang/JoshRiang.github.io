235ebe8383128410618e564fcd611607bd3b908d
Josh <88961456+JoshRiang@users.noreply.github.com>
2022-04-15 21:21:14 +0800

    Update siren.js
    Change siren.gjithub.io to LandunPJR

--- js/siren.js
+++ js/siren.js
@@ -1,8 +1,8 @@
-var Wail = new Audio('https://github.com/JoshRiang/siren.github.io/raw/main/audio/SIREN_1.mp3');
-var Yelp = new Audio('https://github.com/JoshRiang/siren.github.io/raw/main/audio/SIREN_2.mp3');
-var Phaser = new Audio('https://github.com/JoshRiang/siren.github.io/raw/main/audio/POLICE_WARNING.mp3');
-var Horn = new Audio('https://github.com/JoshRiang/siren.github.io/raw/main/audio/AIRHORN_EQD.mp3');
-var HighLow = new Audio('https://github.com/JoshRiang/siren.github.io/raw/main/audio/AMBULANCE_WARNING.mp3');
+var Wail = new Audio('https://github.com/JoshRiang/LandunPJR/raw/main/audio/SIREN_1.mp3');
+var Yelp = new Audio('https://github.com/JoshRiang/LandunPJR/raw/main/audio/SIREN_2.mp3');
+var Phaser = new Audio('https://github.com/JoshRiang/LandunPJR/raw/main/audio/POLICE_WARNING.mp3');
+var Horn = new Audio('https://github.com/JoshRiang/LandunPJR/raw/main/audio/AIRHORN_EQD.mp3');
+var HighLow = new Audio('https://github.com/JoshRiang/LandunPJR/raw/main/audio/AMBULANCE_WARNING.mp3');
 var toggle = false;
 
 // General Function
